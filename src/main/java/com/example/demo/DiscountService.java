b662961e85ec6560dc55f13014c122d7a614cfd0
Markus Nyland <markus.nyland@forbrukerradet.no>
2024-04-11 12:47:29 +0200

    Adjust discount summarization

--- src/main/java/com/example/demo/DiscountService.java
+++ src/main/java/com/example/demo/DiscountService.java
@@ -3,15 +3,14 @@ package com.example.demo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Service
 public class DiscountService {
 
     public int getDiscount(List<Watch> watches) {
-        return IntStream.of(
-                new RolexDiscountStrategy().applyDiscount(watches),
-                new MichaelCorsDiscountStrategy().applyDiscount(watches)
-        ).sum();
+        return  Stream.of(new RolexDiscountStrategy(), new MichaelCorsDiscountStrategy())
+                .mapToInt(discountStrategy -> discountStrategy.applyDiscount(watches))
+                .sum();
     }
 }
